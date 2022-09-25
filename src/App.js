b6a9d415e115e872ace14162f6008af48d5681c8
Ekenedilichukwu <ekenedilichukwu@Ekenedilichukwus-MacBook-Pro.local>
2022-09-25 19:03:46 +0100

    Set up update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <script src="https://kit.fontawesome.com/2fbc753cfc.js" crossorigin="anonymous"></script>
     </div>
   );
 }
---
+++ src/app/ui/pages/Contact.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// Class Component
+const Contact = () => {
+  return <div className="">Contact</div>;
+};
+
+
+export default Contact;
