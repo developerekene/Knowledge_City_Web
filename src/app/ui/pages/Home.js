ce656fc1151dfbbb15b21b1ec7f17ef01ee63dbf
CLARAEKENE <claraekene39@gmail.com>
2022-09-26 18:02:51 -0700

    feature_navbar_inprogress

--- src/app/ui/pages/Home.js
+++ src/app/ui/pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import NavBar from '../components/NavBarMain'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+     <NavBar/>
+   
+    </>
+    
   )
 }
 
