4ded7dfdace7e70aa78b39e2582019b8b36c05b0
CLARAEKENE <claraekene39@gmail.com>
2022-09-29 16:13:55 -0700

    feature_homepage_inprogress

--- src/app/utills/Tools.js
+++ src/app/utills/Tools.js
@@ -6,7 +6,13 @@ export const COLORS = {
     baseColorFive: "#1c2140",
     baseColorSix: "#1976D2",
     baseColorSeven: "#626566",
-    baseColorEight: "#ADD8E6"
+    baseColorEight: "#ADD8E6",
+    baseColorNine: "#F8C471",
+    baseColorTen: "#F6DDCC",
+    baseColorEleven: "#D2B4DE",
+    baseColorTwelve: "#A9CCE3",
+    baseColorThirteen: "#F5EEF8",
+    baseColorFourteen: "#8E44AD"
 }
 
 export const SIZES = {
