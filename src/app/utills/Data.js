1bfe39592f99c75b5a34fb5d0a684dbf9efb73d2
CLARAEKENE <claraekene39@gmail.com>
2022-09-30 17:01:28 -0700

    feature_homepage_continues

--- src/app/utills/Data.js
+++ src/app/utills/Data.js
@@ -14,5 +14,6 @@ export const DUMMYDATA = [
 ]
 
 export const TEXT = {
-    welcomeText: "You are welcome to Knowledge City"
+    welcomeText: "You are welcome to Knowledge City",
+    homeHeaderText: "Limitless learning at your fingertips"
 }
